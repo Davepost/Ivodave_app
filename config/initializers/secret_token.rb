4832821e605959c11cb731dfcf1253d7be9273db
Davepost <davepost1990@hotmail.com>
2012-03-14 19:08:49 +0000

    Latest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IvodaveApp::Application.config.secret_token = 'bda9685971f11aa96906058a5b90db6fc5fe80c22cdaf6a310a7f0ee8547fe321f8e9fdf994cf228fa2c8b4c37bd6c9da57d9f7b0ed9dacb7bb7221bcb0f109f'
